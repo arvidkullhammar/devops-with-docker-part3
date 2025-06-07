c6cc6983fa31fd34ad33c74ec9cc17471d0f7f18
Arvid Kullhammar <arvidkullhammar@gmail.com>
2025-06-07 13:58:14 +0200

    updated port app is running on

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello Arvid!</h1>')
 })
 
-const PORT = 8080
+const PORT = 3000
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
